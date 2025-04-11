0095ecf54dae6320828f6afdab6fa8681e6ae0e2
DanaArnonPerry <dana@2d-point.com>
2025-04-11 20:21:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -366,7 +366,7 @@ with tab1:
             title="מדד החירות הדיגיטלי",
             title_font=dict(size=16, color="#8000FF", family="Rubik, sans-serif"),  # גופן רוביק
             tickfont=dict(size=14, color="#333333", family="Rubik, sans-serif"),  # גופן רוביק
-            title_standoff=30,
+            title_standoff=60,
             gridcolor='rgba(200,200,200,0.5)',
             zeroline=False,
             domain=[0.02, 0.98],  # הגדלת אזור הגרף למעלה ולמטה
